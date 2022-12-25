a77a7c1f04e786081ff759f217dce30784f75633
Vlvdik <114529141+Vlvdik@users.noreply.github.com>
2022-12-25 23:20:46 +0300

    Update methods.go

--- internal/telegram/methods.go
+++ internal/telegram/methods.go
@@ -4,7 +4,29 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"io/ioutil"
 	"log"
+	"os"
 )
+
+func convertPhoto(filepath string) tgbotapi.FileBytes {
+	photo, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return tgbotapi.FileBytes{
+		Name:  "picture",
+		Bytes: photo,
+	}
+}
+
+func deletePhoto(filepath string) error {
+	err := os.Remove(filepath)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func sendMessage(bot *tgbotapi.BotAPI, chat int64, msgID int, text string) {
 	res := tgbotapi.NewMessage(chat, text)
@@ -17,20 +39,17 @@ func sendMessage(bot *tgbotapi.BotAPI, chat int64, msgID int, text string) {
 }
 
 func sendPhoto(bot *tgbotapi.BotAPI, chat int64, msgID int) {
-	photo, err := ioutil.ReadFile("internal/upload/picture.jpg")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	photoFileBytes := tgbotapi.FileBytes{
-		Name:  "picture",
-		Bytes: photo,
-	}
+	photoFileBytes := convertPhoto("internal/upload/обэмэ.jpg")
 
 	uploadedPhoto := tgbotapi.NewPhoto(chat, photoFileBytes)
 	uploadedPhoto.ReplyToMessageID = msgID
 
-	_, err = bot.Send(uploadedPhoto)
+	_, err := bot.Send(uploadedPhoto)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = deletePhoto("internal/upload/обэмэ.jpg")
 	if err != nil {
 		log.Fatal(err)
 	}
