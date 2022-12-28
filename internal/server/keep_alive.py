fc32db0b507001a90d6570040f7f2a3de0de9c11
Vlvdik <114529141+Vlvdik@users.noreply.github.com>
2022-12-28 23:45:36 +0300

    Create keep_alive.py

---
+++ internal/server/keep_alive.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+
+@app.route('/')
+def main():
+    return "<h1>Still alive...</h1>"
+
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
