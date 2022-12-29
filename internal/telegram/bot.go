98f52f7fe6d0def53fac6d801fd7a41717e3a97c
Vlvdik <114529141+Vlvdik@users.noreply.github.com>
2022-12-29 20:00:49 +0300

    Update bot.go

--- internal/telegram/bot.go
+++ internal/telegram/bot.go
@@ -3,43 +3,81 @@ package bot
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
+	"strings"
+	"time"
 )
 
-func listen(bot *tgbotapi.BotAPI, updates tgbotapi.UpdatesChannel) {
-	for update := range updates {
-		if update.Message != nil {
-			// frequently used params
-			username := update.Message.From.UserName
-			chat := update.Message.Chat.ID
-			msg := update.Message.Text
-			msgID := update.Message.MessageID
-
-			log.Printf("[%s] %s", username, msg)
-
-			switch msg {
-			case "/ping":
-				sendMessage(bot, chat, msgID, "pong")
-			case "/send":
-				sendPhoto(bot, chat, msgID)
-			default:
-				sendMessage(bot, chat, msgID, "Unknown command")
-			}
-		}
-	}
+type handlers interface {
+	sendMessage(chat int64, msgID int, text string)
+	sendPhoto(chat int64, msgID int, text string)
 }
 
-func Init(token string) {
+type methods interface {
+	Start() error
+	initUpdates() tgbotapi.UpdatesChannel
+	handleUpdates(updates tgbotapi.UpdatesChannel)
+}
+
+type Functions interface {
+	handlers
+	methods
+}
+
+type TelegramBot struct {
+	bot *tgbotapi.BotAPI
+	Functions
+}
+
+func NewTelegramBot(token string) *TelegramBot {
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	bot.Debug = true
+	return &TelegramBot{bot: bot}
+}
 
+func (tb *TelegramBot) initUpdates() tgbotapi.UpdatesChannel {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	updates := bot.GetUpdatesChan(u)
+	return tb.bot.GetUpdatesChan(u)
+}
 
-	listen(bot, updates)
+func (tb *TelegramBot) handleUpdates(updates tgbotapi.UpdatesChannel) {
+	for update := range updates {
+		if update.Message != nil {
+			// frequently used params
+			username := update.Message.From.UserName
+			chat := update.Message.Chat.ID
+			words := strings.Split(update.Message.Text, " ")
+			msg := update.Message.Text
+			msgID := update.Message.MessageID
+
+			log.Printf("[%s] %s", username, msg)
+
+			switch words[0] {
+			case "/ping":
+				tb.sendMessage(chat, msgID, "pong")
+			case "/send":
+				if len(words) > 1 {
+					tb.sendMessage(chat, msgID, "Ожидайте, ваш запрос принят")
+					go tb.sendPhoto(chat, msgID, msg)
+				} else {
+					tb.sendMessage(chat, msgID, errInvalidPrompt)
+				}
+			default:
+				tb.sendMessage(chat, msgID, errUnknownCommand)
+			}
+		}
+	}
+}
+
+func (tb *TelegramBot) Start() error {
+	log.Printf("%s starts working\nStart time: %v", tb.bot.Self.UserName, time.Now().Format(time.RFC822))
+
+	updates := tb.initUpdates()
+	tb.handleUpdates(updates)
+
+	return nil
 }
