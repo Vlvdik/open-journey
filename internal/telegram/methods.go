4c0cbc07881e8f6ea4bb5bacc46f44f182baba2d
Vlvdik <114529141+Vlvdik@users.noreply.github.com>
2022-12-30 23:31:29 +0300

    Update methods.go

--- internal/telegram/methods.go
+++ internal/telegram/methods.go
@@ -13,6 +13,27 @@ func getPrompt(text string) string {
 	return strings.Join(strings.Split(text, " ")[1:], "")
 }
 
+func useImagine(imgChn chan string, prompt string) {
+	script := "from app.model import imagine; print(imagine('" + prompt + "'))"
+	cmd := exec.Command("python", "-c", script)
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	imgChn <- string(out)
+}
+
+
+func IsPrompt(text string) bool {
+	if len(text) > 1 {
+		return true
+	} else {
+		return false
+	}
+}
+
 func GetPromptURL(text string) (string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 150*time.Second)
 	defer cancel()
@@ -30,16 +51,4 @@ func GetPromptURL(text string) (string, error) {
 	case <-ctx.Done():
 		return "", errors.New(ErrImagineTimeOut)
 	}
-}
-
-func useImagine(imgChn chan string, prompt string) {
-	script := "from app.model import imagine; print(imagine('" + prompt + "'))"
-	cmd := exec.Command("python", "-c", script)
-
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	imgChn <- string(out)
 }
