aec0104d6691911bca0ae1efe8633414ff92ffd3
Vladislav <114529141+Vlvdik@users.noreply.github.com>
2023-01-16 05:31:52 +0300

    Update errors.go

--- internal/telegram/errors.go
+++ internal/telegram/errors.go
@@ -1,7 +1,8 @@
 package bot
 
 const (
-	ErrUnknownCommand = "Invalid Command"
-	ErrInvalidPrompt  = "Invalid Prompt"
-	ErrImagineTimeOut = "Prompt timeout"
+	ErrUnknownCommand    = "Invalid Command"
+	ErrInvalidPrompt     = "Invalid Prompt"
+	ErrImagineTimeOut    = "Prompt timeout"
+	ErrTranslationFailed = "Error when trying to translate"
 )
